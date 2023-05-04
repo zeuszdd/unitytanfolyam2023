1e998e4925bd78da97f62d73f062c8ec817df2ee
Balázs Solti <zeuszdd@freemail.hu>
2023-05-04 16:19:51 +0200

    050401

--- Assets/scripts/Homework.cs
+++ Assets/scripts/Homework.cs
@@ -1,8 +1,85 @@
+using System;
 using UnityEngine;
 
 
 public class Homework : MonoBehaviour
 {
+    int SzamjegyOsszeg(int n)
+    {
+        int sum=0;
+        while (n!=0)
+        {
+            int szamjegy=n%10;
+            sum+=szamjegy;
+            n/=10;
+        }
+        return sum;
+    }
+
+    int[] sorrend(int[] array)
+    {
+        if(array==null)
+        {
+            return null;
+        }
+        int[] newArray = new int[array.Length];
+        for(int i=0; i<array.Length;i++)
+        {
+            int i2=array.Length-1-i;
+            newArray[i]=array[i2];
+        }
+        return newArray;
+    }
+
+    int[] Fibonacci(int length)
+    {
+        int[] result = new int[length];
+
+        if(length>0)
+        {
+            result[0]=0;
+        }
+        if(length>1)
+        {
+            result[1]=1;
+        }
+        for(int i=2; i<length, i++)
+        {
+            result[i]=result[i-1]+result[i-2];
+        }
+        return result;
+    }
+
+    int Betuszamlalo(string s)
+    {
+        List<char> characters = new List<char>();
+        foreach (char c in s)
+        {
+            if(!characters.Contains(c))
+            {
+                characters.Add(c)
+            }
+        }
+        return characters.Count;
+
+    }
+
+    class NormalizedDirectionVector : MonoBehaviour
+    {
+        [SerializeField] Vector2 a, b;
+
+        [SerializeField] int stepCount;
+        [SerializeField] Vector2 step;
+
+        void OnValidate()
+        {
+            Vector2 dirVec=b-a;
+            float tavolsag=dirVec.magnitude;
+            stepCount=MathF.CeilToInt(distance/1f);
+            step=dirVec/stepCount;
+        }
+    }
+    
     /*
     // [SerializeField] int a;
     // [SerializeField] int b;
